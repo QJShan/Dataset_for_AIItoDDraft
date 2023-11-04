059c9e0c7ca5171b5ce61644069c110c67cb4139
YXShan <131762142+QJShan@users.noreply.github.com>
2023-11-04 09:38:26 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,16 @@
-print("ok")
+import uvicorn
+from fastapi import FastAPI,Body
+
+app = FastAPI()
+
+
+@app.post("/test")
+def Chat(
+        only_retrive: bool = Body(default=True),
+        name: str = Body(default="hello docker",title="access private or public resources"),
+):
+    return {"only_retrive":only_retrive,"name":name}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app,host="0.0.0.0",reload=False,port=8001)
